6bfd24451ddcdc379075309ee74081dac3324b6e
Luis Melgarejo <ti13@emilima.com.pe>
2022-02-20 04:14:24 -0500

    Feat: Agregando vue.min.js y creando lógica para 2 idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,42 @@ Particles.init({
         }
     }]
 });
+
+
+let data = null;
+let langs = ["en", "es"]
+let gLang = 'es'
+
+const readDataJson = async(callback) => {
+    try {
+        const response = await fetch('/assets/data.json')
+        const json = await response.json()
+        console.log(json);
+        data = json;
+        callback(data)
+    } catch (error) {
+        // catch error
+    }
+}
+
+const loadLangFromLS = () => {
+    const lang = localStorage.getItem("lang")
+    if (!lang) return
+    if (!langs.includes(lang)) return
+    gLang = lang
+}
+
+
+
+loadLangFromLS();
+readDataJson((json) => {
+    const { menu } = json
+    const instance = new Vue({
+        el: "#app",
+        data: {
+            lang: gLang,
+            menu: menu
+        },
+        mounted() {} // on mounted
+    })
+})
